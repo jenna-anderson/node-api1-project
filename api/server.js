175944f018eb9acc63c5f20887d879f193c41375
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-05 15:06:19 -0600

    completes put request to edit a user

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ server.use(express.json())
 
 // ENDPOINTS
 
-// [POST] /api/users (created a new user)
+// [POST] /api/users (creates a new user and returns the new user)
 server.post('/api/users', (req, res) => {
     const { id, name, bio } = req.body;
     if(!name || !bio) {
@@ -61,7 +61,7 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
-// [DELETE] /api/users/:id (deletes user with :id)
+// [DELETE] /api/users/:id (deletes user with :id and returns the deleted user)
 server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
     User.remove(id)
@@ -78,6 +78,30 @@ server.delete('/api/users/:id', (req, res) => {
             custom: err.message
         })
     })
+})
+
+// [PUT] /api/users/:id (updates an existing user and returns the updated user)
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    if(!name || !bio) {
+        res.status(400).json({message: "Please provide name and bio for the user"})
+    } else{
+        User.update(id, { name, bio })
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user information could not be modified",
+                custom: err.message 
+            })
+        })
+    }
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}

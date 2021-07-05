5c57a575eda3d4867afd433737c10aa9431d6cc7
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-05 14:29:58 -0600

    completes post request creating a new user

--- api/server.js
+++ api/server.js
@@ -1,3 +1,31 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+// ENDPOINTS
+
+// [POST]
+server.post('/api/users', (req, res) => {
+    const { id, name, bio } = req.body;
+    if(!name || !bio) {
+        res.status(400).json({message: "Please provide name and bio for the user"})
+    }
+    else{
+        User.insert({ id, name, bio })
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the user to the database",
+                custom: err.message
+            })
+        })
+    }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
